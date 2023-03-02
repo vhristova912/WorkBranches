414b93d6e197d265fa7139492f5b8ec3cd88578e
vhristova912 <viki66hristova@gmail.com>
2023-03-02 11:15:39 +0200

    zad

---
+++ StatisticArray/StatisticArray/OptionsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticArray
+{
+   public static class OptionsManager
+    {
+        public static int CountLess100(int[] arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+
+        public static int MaxNum(int[] arr)
+        {
+            return arr.Max();
+        }
+
+        public static int[] ReverseArr(int[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+
+        public static void PrintArr(int[]arr)
+        {
+            Console.WriteLine(string.Join(", ",arr));
+        }
+    }
+}
---
+++ StatisticArray/StatisticArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StatisticArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(OptionsManager.CountLess100(arr));
+            Console.WriteLine(OptionsManager.MaxNum(arr));
+            arr=OptionsManager.ReverseArr(arr);
+            OptionsManager.PrintArr(arr);
+            // TO DO
+        }
+    }
+}
